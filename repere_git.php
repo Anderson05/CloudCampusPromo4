984ab541459f8927846a8f1a0eb5b6fe91652ca5
Anderson05 <nguessiandre@yahoo.fr>
2022-09-19 08:17:52 -0500

    Contact created + generation CRUD Contact

---
+++ repere_git.php
@@ -0,0 +1,22 @@
+#Commandes Git
+
+git clone https://github.com/Anderson05/CloudCampusPromo4.git
+
+cd CloudCampusPromo4
+
+# mettre a jour les sources locales
+git pull
+
+git branch nom_de_ma_branche
+
+git checkout -b nom_de_ma_branche
+
+# Status
+git Status
+
+git add .
+#git add src/EventListener
+
+git commit -m "ce que vous avez fait"
+
+git push
